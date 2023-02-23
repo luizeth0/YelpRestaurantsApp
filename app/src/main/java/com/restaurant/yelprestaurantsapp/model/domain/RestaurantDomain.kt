48908571fe7033f054bd80333ed291a0b7e6a8ba
luizeth0 <luis.hemi94@gmail.com>
2023-02-22 21:04:45 -0500

    Created model & domain

---
+++ app/src/main/java/com/restaurant/yelprestaurantsapp/model/domain/RestaurantDomain.kt
@@ -0,0 +1,33 @@
+package com.restaurant.yelprestaurantsapp.model.domain
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.restaurantsapp.model.Business
+import com.example.restaurantsapp.model.Location
+
+@Entity(tableName = "restaurants")
+data class RestaurantDomain(
+    @PrimaryKey
+    val id: String,
+    val image: String,
+    val name: String,
+    val phone: String,
+    val price: String,
+    val rating: Double,
+    val location: Location,
+    val distance: Double
+)
+
+fun List<Business>?.mapToDomainRestaurants(): List<RestaurantDomain> =
+    this?.map {
+        RestaurantDomain(
+            id = it.id ?: "ID not available",
+            image = it.imageUrl ?: "not available",
+            name = it.name ?: "not available",
+            phone = it.displayPhone ?: "not available",
+            price = it.price ?: "not available",
+            rating = it.rating ?: 0.0,
+            location = it.location ?: Location(address1 = "not available"),
+            distance = it.distance ?: 0.0
+        )
+    } ?: emptyList()
